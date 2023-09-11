010de36e31a7c7bb1707d39a299725c7c82c6aad
Osama <osama.abdo4040@gmail.com>
2023-09-11 16:44:13 +0300

    finished home screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_islami_app/home/home_screen.dart';
+import 'package:flutter_islami_app/my_theme.dart';
 
 void main(){
   runApp(MyApp());
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
       routes: {
         HomeScreen.routeName : (context) => HomeScreen(),
       },
+      theme: MyTheme.lightTheme,
     );
   }
 }
