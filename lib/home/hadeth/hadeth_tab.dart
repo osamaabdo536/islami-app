50234c118e5d1e153a325c4163f45db702260f8d
Osama <osama.abdo4040@gmail.com>
2023-09-15 20:36:31 +0300

    Edited hadeth tab.

--- lib/home/hadeth/hadeth_tab.dart
+++ lib/home/hadeth/hadeth_tab.dart
@@ -1,63 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_islami_app/home/hadeth/item_hadeth_name.dart';
-import 'package:flutter_islami_app/my_theme.dart';
 
-class HadethTab extends StatelessWidget {
 
-  List<String> names = [
-    'الحديث رقم 1',
-    'الحديث رقم 2',
-    'الحديث رقم 3',
-    'الحديث رقم 4',
-    'الحديث رقم 5',
-    'الحديث رقم 6',
-    'الحديث رقم 7',
-    'الحديث رقم 8',
-    'الحديث رقم 9',
-    'الحديث رقم 10',
-    'الحديث رقم 11',
-    'الحديث رقم 12',
-    'الحديث رقم 13',
-    'الحديث رقم 14',
-    'الحديث رقم 15',
-    'الحديث رقم 16',
-    'الحديث رقم 17',
-    'الحديث رقم 18',
-    'الحديث رقم 19',
-    'الحديث رقم 20',
-    'الحديث رقم 21',
-    'الحديث رقم 22',
-    'الحديث رقم 23',
-    'الحديث رقم 24',
-    'الحديث رقم 25',
-    'الحديث رقم 26',
-    'الحديث رقم 27',
-    'الحديث رقم 28',
-    'الحديث رقم 29',
-    'الحديث رقم 30',
-    'الحديث رقم 31',
-    'الحديث رقم 32',
-    'الحديث رقم 33',
-    'الحديث رقم 34',
-    'الحديث رقم 35',
-    'الحديث رقم 36',
-    'الحديث رقم 37',
-    'الحديث رقم 38',
-    'الحديث رقم 39',
-    'الحديث رقم 40',
-    'الحديث رقم 41',
-    'الحديث رقم 42',
-    'الحديث رقم 43',
-    'الحديث رقم 44',
-    'الحديث رقم 45',
-    'الحديث رقم 46',
-    'الحديث رقم 47',
-    'الحديث رقم 48',
-    'الحديث رقم 49',
-    'الحديث رقم 50',
-  ];
+class HadethTab extends StatefulWidget {
+  @override
+  State<HadethTab> createState() => _HadethTabState();
+}
+
+class _HadethTabState extends State<HadethTab> {
+  List<Hadeth> ahadethList = [];
+
   @override
   Widget build(BuildContext context) {
+    if(ahadethList.isEmpty){
+      loadHadethFile();
+    }
     return Center(
       child: Column(
         children: [
@@ -66,13 +24,18 @@ class HadethTab extends StatelessWidget {
             color: Theme.of(context).primaryColor,
             thickness: 3,
           ),
-          Text('Ahadeth' ,
+          Text('Hadeth Name' ,
             style: Theme.of(context).textTheme.titleMedium,
           ),
           Divider(
             color: Theme.of(context).primaryColor,
             thickness: 3,
           ),
+          ahadethList.isEmpty ?
+              Center(child: CircularProgressIndicator(
+                color: Theme.of(context).primaryColor,
+              ))
+              :
           Expanded(
             flex: 2,
             child: ListView.separated(
@@ -83,9 +46,9 @@ class HadethTab extends StatelessWidget {
                 );
               },
                 itemBuilder: (context,index){
-                  return ItemHadethName(name: names[index],index: index) ;
+                  return ItemHadethName(hadeth: ahadethList[index],) ;
                 },
-              itemCount: names.length,
+              itemCount: ahadethList.length,
             ),
           ),
 
@@ -93,4 +56,25 @@ class HadethTab extends StatelessWidget {
       ),
     );
   }
+
+  void loadHadethFile()async{
+    String ahadethContent = await rootBundle.loadString('assets/files/ahadeth.txt');
+    List<String> hadethList = ahadethContent.split('#\r\n');
+    for(int i = 0 ; i < hadethList.length ; i++){
+      List<String> hadethLines = hadethList[i].split('\n');
+      String title = hadethLines[0];
+      hadethLines.removeAt(0);
+      Hadeth hadeth = Hadeth(title: title, content: hadethLines);
+      ahadethList.add(hadeth);
+      setState(() {
+
+      });
+    }
+  }
+}
+class Hadeth{
+  String title;
+  List<String> content;
+
+  Hadeth({required this.title,required this.content});
 }
